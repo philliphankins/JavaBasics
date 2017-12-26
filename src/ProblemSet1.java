4a54ecc9d378012dd18ec4de8c3c305765c91c09
philliphankins <34249287+philliphankins@users.noreply.github.com>
2017-12-25 19:07:59 -0700

    Answer to question #1

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -3,7 +3,16 @@ public class ProblemSet1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Merry Cristmas!");
+		/*Question #1
+Which of the following Java variable declarations has an error?
+A. int x = 5;
+B. double temperature = 75.6;
+C. char grade = ’A’;
+D. String name = ’Adam’
+		 **/
+		System.out.println("Answer #1. D. String Name = Adam");
+		System.out.println("Double qoutes are for text, single qoutes are for characters.");
+		System.out.print(" ");
 
 	}
 
