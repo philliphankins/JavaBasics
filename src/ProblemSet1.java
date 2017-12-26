1e69f36259618e34e972153370b846dd0b852fec
philliphankins <34249287+philliphankins@users.noreply.github.com>
2017-12-25 19:15:38 -0700

    Answer to Question #2

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -12,7 +12,16 @@ D. String name = ’Adam’
 		 **/
 		System.out.println("Answer #1. D. String Name = Adam");
 		System.out.println("Double qoutes are for text, single qoutes are for characters.");
-		System.out.print(" ");
+		System.out.println();
+		/*Question 2
+What value for register will be printed at the end of this block of Java code? double register = 10.0; register = register + 5; //Customer pays $5. register = register - 2.5; //Customer receives $2.50 as change. register = register + 10; //Customer pays $10. register = register - 3; //Customer receives $3 as change. System.out.println(register);
+A. 19.0
+B. 19.5
+C. 22.5
+D. 25.5
+**/
+		System.out.println("Answer #2 B. 19.5");
+		System.out.println();
 
 	}
 
