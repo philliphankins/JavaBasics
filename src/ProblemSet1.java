a0f2d7bf010c55afe28e9635ad2da149b0c91dd7
philliphankins <34249287+philliphankins@users.noreply.github.com>
2017-12-25 19:31:56 -0700

    Answer to question #3

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -22,7 +22,15 @@ D. 25.5
 **/
 		System.out.println("Answer #2 B. 19.5");
 		System.out.println();
-
+		/*Question 3
+* Define an integer variable called bankBalance. Initialize it to a value of 500.
+* Then add 250 to it. Then subtract 100 from it. Finally, print the resulting value.
+**/
+		System.out.println("Answer #3");
+		int bankBalance = 500;
+				bankBalance = bankBalance + 250;
+				bankBalance = bankBalance - 100;
+				System.out.println(bankBalance);
+				System.out.println("Please see code for programing used to achieve answer.");
 	}
-
 }
