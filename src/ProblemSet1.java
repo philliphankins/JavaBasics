eb720b02e21c87bf26f976585821fc3d9e20ce34
philliphankins <34249287+philliphankins@users.noreply.github.com>
2017-12-25 20:40:44 -0700

    Answer to question 6

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -58,7 +58,29 @@ Give month and day appropriate values for your birthday
 		System.out.println(day);
 		System.out.println();
 		
-				
+/* Question 6
+Write Java code to create a String variable called firstName, define it to be your first name as a String. 
+Then define a variable called lastName and define it to be your last name as a String. Then dfine a variable called fullName and 
+set it to be your first name followed by a space followed by your last name. 
+Use the existing variables for your first and last name and String concatenation to define fullName. Finally, write code to print this text:
+Hello, my name is [full name]. There are [number] letters in my name.
+Use String concatenation to create the first String to print using the fullName variable, 
+and use the .length() command on firstName and lastName to calculate the number of letters.
+Note: you can concatenate an integer and a String and the integer will be converted to a String. For example, this expression:
+"There are "+ 7 + "days in a week."
+will be evaluated as the String:
+"There are 7 days in a week."
+*/
+		
+		String firstName = "Phillip";
+		String lastName = "Hankins";
+		String fullName = firstName + " " + lastName;
+		
+		System.out.println("Question #6, check program for actual code");
+		System.out.println();		
+		System.out.println("Hello, my name is " + fullName +".");
+		int lettersinName = firstName.length() + lastName.length();
+		System.out.println("There are " + lettersinName + " letters in my name.");
 		
 					
 	
