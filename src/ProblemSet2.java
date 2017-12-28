9ce1a46c402999e12b8a2241b41f4a663ae947cc
philliphankins <34249287+philliphankins@users.noreply.github.com>
2017-12-27 23:36:59 -0700

    Answer to Question #6

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -150,11 +150,48 @@ System.out.println("timeOfDay = \"daytime\";");
 System.out.println("} else {");
 System.out.println("timeOfDay = \"night\";");
 System.out.println();
+/*Question 6
+Assume you have access to an integer variable called dayOfTheWeek and a boolean variable
+called holiday, which is true when it is a holiday and false on normal days. Write Java
+code that prints “Wake up at 7:00” on weekdays that are not holidays, and prints “Sleep
+in!” on weekends and holidays. For the variable dayOfTheWeek, use this key:
+1 = Monday
+2 = Tuesday
+3 = Wednesday
+4 = Thursday
+5 = Friday
+6 = Saturday
+7 = Sunday
+Starting code:
+//Assume these could have any value:
+int weekday = 5;
+boolean holiday = false;
+
+*/
+
+System.out.println("Answer to Question #6.");
+System.out.println();
+int weekday = 5;
+boolean holiday = false;
+
+if (weekday >=1 && weekday <= 5 && !holiday) {
+	System.out.println("Wake up at 7:00.");
+} else {
+	System.out.println("Sleep in!"); 
+}
+System.out.println("if (weekday >=1 && weekday <= 5 && !holiday) {);");
+System.out.println("System.out.println(\"Wake up at 7:00.\");");
+System.out.println("} else {");
+System.out.println("System.out.println(\"Sleep in!\");");
+System.out.println();
+
+	
+}
 
 }
 	
 	
-	}
+	
 	
 	
 
