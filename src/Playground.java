3e4cecc9c3df4b4152f447a2b136ad726ced32dc
philliphankins <34249287+philliphankins@users.noreply.github.com>
2017-12-27 20:49:35 -0700

    Answer to question #1.

---
+++ src/Playground.java
@@ -0,0 +1,20 @@
+
+public class Playground {
+
+	
+	public static void main(String[] args) {
+		
+String s = doopDeedoo();
+System.out.println(s);
+		}
+
+		public static String doopDeedoo() {
+			String s = "Doop Dee Doo!";
+					return s;
+			
+			
+			
+		}
+	}
+
+
---
+++ src/ProblemSet2.java
@@ -0,0 +1,34 @@
+
+public class ProblemSet2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Question 1
+What will the following block of Java code print?
+
+double balance = 0;
+balance = balance + 20; //Add quarter 1 profits (thousands).
+balance = balance - 25; //Subtract quarter 1 expenses (thousands).
+balance = balance + 30; //Add quarter 2 profits (thousands).
+balance = balance - 25; //Subtract quarter 1 expenses (thousands).
+if (balance < 0) {
+System.out.println("We’re in the red!");
+} else if (balance > 0) {
+System.out.println("We made a profit!");
+} else {
+System.out.println("We broke even.");
+
+A. We’re in the red!
+B. We made a profit!
+C. We broke even.
+}
+*/
+	System.out.println("Question #1.");	
+	System.out.println("C. We broke even");	
+
+		
+		
+
+	}
+
+}
