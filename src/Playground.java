40e376934c8515fd54e069442371878140adf817
philliphankins <34249287+philliphankins@users.noreply.github.com>
2017-12-29 15:13:18 -0700

    Yadda yadda

--- src/Playground.java
+++ src/Playground.java
@@ -1,20 +1,35 @@
 
 public class Playground {
 
-	
-public static void main(String[] args) {
-	
-		
 
 
 
-System.out.println("test");
+public static void main(String[] args)  {}
+
+public double makeChange(double itemCost, double dollarsProvided) {
+double returnedChange = makeChange(3.60,5.75);
+double change = dollarsProvided - itemCost;
+
+System.out.println(returnedChange);
+return change;
+
+}
+
+
 
 
 
 }
+
+
+
+
+
+
+
+
 	
-}			
+			
 			
 		
 
