46912aef185ede156a26c671f5ce2da71150ccbf
philliphankins <34249287+philliphankins@users.noreply.github.com>
2017-12-30 16:56:45 -0700

    Yadda

--- src/Playground.java
+++ src/Playground.java
@@ -4,23 +4,25 @@ public class Playground {
 
 public static void main(String[] args)  {
 
-} 
+public int keepRolling() {
+    int dice1 = rollDice();
+    int dice2 = rollDice();
+    int dice3 = rollDice();
+    int dice4 = rollDice();
+    int dice5 = rollDice();
+    int count = 1;
+    while (!(dice1 == dice2 && dice2 == dice3 && dice3 == dice 4 && dice 4 == dice 5)){
+        
+        dice1 = rollDice();
+        dice2 = rollDice();
+        dice3 = rollDice();
+        dice4 = rollDice();
+        dice5 = rollDice();
+        count = count + 1;
+    }
+    return count;
 
-public int rollDice() {
 
-
-double randomNumber = Math.random();
-randomNumber = randomNumber * 6;
-randomNumber = randomNumber + 1;
-int randomInt = (int)randomNumber;
-
-int roll1 = rollDice();
-int roll2 = rollDice();
-
-System.out.println("Roll 1: " + roll1);
-System.out.println("Roll 2: " + roll2);
-
-return randomInt;
 
 }
 
