ad344f240849df9df2f597daf70e21e294e9909d
philliphankins <34249287+philliphankins@users.noreply.github.com>
2017-12-29 23:17:46 -0700

    Answer to question #1

--- src/ProblemSet3.java
+++ src/ProblemSet3.java
@@ -3,6 +3,19 @@ public class ProblemSet3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+/*Question 1
+In the function signature below, what is the return type?
+public float squareRoot(int x)
+A. public
+B. float
+C. squareRoot
+D. int
+*/
+		
+		System.out.println("Answer to Question #1.");
+		System.out.println("B. Float");
+		System.out.println();
 
 	}
 
