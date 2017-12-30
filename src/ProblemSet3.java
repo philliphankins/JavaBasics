5cb5112557071aeedaa896d6659b0bbba6d0109c
philliphankins <34249287+philliphankins@users.noreply.github.com>
2017-12-29 23:22:08 -0700

    Answer to Question #2

--- src/ProblemSet3.java
+++ src/ProblemSet3.java
@@ -16,6 +16,14 @@ D. int
 		System.out.println("Answer to Question #1.");
 		System.out.println("B. Float");
 		System.out.println();
+		
+/*Write the signature of a function called isPrime(). The access modifier should be public,
+the return type should be boolean, and it should take a single integer parameter.
+*/
+		
+		System.out.println("Answer to Question #2.");
+		System.out.println("public boolean isPrime(int n");
+		System.out.println();
 
 	}
 
