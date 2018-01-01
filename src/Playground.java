70a97d8d2a7e431ce193e4ee1ef463adc03e73f2
philliphankins <34249287+philliphankins@users.noreply.github.com>
2018-01-01 11:41:01 -0700

    Answer to Question#1

--- src/Playground.java
+++ src/Playground.java
@@ -4,23 +4,7 @@ public class Playground {
 
 public static void main(String[] args)  {
 
-public int keepRolling() {
-    int dice1 = rollDice();
-    int dice2 = rollDice();
-    int dice3 = rollDice();
-    int dice4 = rollDice();
-    int dice5 = rollDice();
-    int count = 1;
-    while (!(dice1 == dice2 && dice2 == dice3 && dice3 == dice 4 && dice 4 == dice 5)){
-        
-        dice1 = rollDice();
-        dice2 = rollDice();
-        dice3 = rollDice();
-        dice4 = rollDice();
-        dice5 = rollDice();
-        count = count + 1;
-    }
-    return count;
+
 
 }
 
@@ -28,7 +12,7 @@ public int keepRolling() {
 
 
 
-}
+
 
 
 
---
+++ src/ProblemSet4.java
@@ -0,0 +1,27 @@
+
+public class ProblemSet4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+/*Question 1
+What will be printed by this block of Java code?
+int n = 10;
+while (n < 50) {
+n = n * 2;
+}
+System.out.println(n);
+A. 10
+B. 40
+C. 50
+D. 80
+*/
+		
+		System.out.println("Answer to Question #1");
+		System.out.println();
+		System.out.println("D. 80");
+		System.out.println();
+		
+		
+	}
+
+}
