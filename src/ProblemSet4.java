b515f0b74ca3a1a2d4a7711005a99c3b2f60af5d
philliphankins <34249287+philliphankins@users.noreply.github.com>
2018-01-01 11:50:09 -0700

    Answer to question #2

--- src/ProblemSet4.java
+++ src/ProblemSet4.java
@@ -21,7 +21,31 @@ D. 80
 		System.out.println("D. 80");
 		System.out.println();
 		
+/*Complete the factorial() function below. It should return the product of all the numbers
+from 1 to the parameter n. For example, factorial(5) should return 120 because 1 x 2 x
+3 x 4 x 5 = 120. Think about what kind of loop you want to use to accomplish this.
+Starting code:
+public int factorial(int n) {
+}
+*/
+		
+
+		System.out.println("Answer to Question #2");
+		System.out.println();
+		System.out.println("public int factorial(int n) {");
+		System.out.println("int factorial = 1;");
+		System.out.println("for (int i = 1; i <= n; i++) {");
+		System.out.println("factorial = factorial * i;");
+		System.out.println("}");
+		System.out.println("return factorial;");
+		System.out.println("}");
+		System.out.println();
+		
+		
+		
+		
+	}
 		
 	}
 
-}
+
