58a80d73b4a1c9a5f69f5191c7a9cc1c36ec356f
philliphankins <34249287+philliphankins@users.noreply.github.com>
2018-01-02 18:48:05 -0700

    Answer to question #8.
    Includes the answers to the two challange questions.

--- src/ProblemSet4.java
+++ src/ProblemSet4.java
@@ -206,9 +206,97 @@ return -1;
 		System.out.println();
 		
 		
+/* Question 8
+Let’s improve the monopolyRoll() function from the previous problem set. Recall that in
+Monopoly, players roll two six-sided dice to determine their move. If the same value is on
+both dice, this is called “rolling doubles,” and it means they go again. In the last problem
+set, you wrote a function that rolled two six-sided dice and, if the values on both die were
+the same, rolled two more and returned the sum. This time, write a function that does the
+same except it continues rolling until two non-equal values appear on the two dice. In other
+words, the function should behave as follows:
+1. Generate two random numbers in the 1 to 6 range.
+2. If they are not the same, return the sum of all numbers rolled so far.
+3. If they are the same, keep track of the total rolled so far and return to step 1.
+Think about what kind of loop you want to define to repeat these steps. Again, you may
+want to define a separate function for generating dice rolls.
+Optional challenge: in Monopoly, if a player rolls doubles three times in a row, they go
+to “jail.” Modify your function to keep track of the number of rolls made so far. If three
+consecutive doubles are rolled, return -1 instead of continuing to roll.
+Optional challenge 2: in the last lesson, you learned about while loops and for loops as a
+way to make a computer program repeat. There is another method for making a program
+repeat using only functions called recursion. A “recursive” function is one which calls itself
+inside itself. See if you can write the monopolyRoll() function by calling monopolyRoll()
+inside the function itself to handle the case where further rolls are made.
+*/
 		
+		System.out.println("Answer to Question #8.");
+		System.out.println("");
+		System.out.println("public int diceRoll(int sides) {");
+		//This expression generates a random double in the interval
+		//[0, sides).
+		System.out.println("double randomNumber = Math.random() * sides;");
+		//Our random number is now in the interval [1, sides + 1)
+		System.out.println("randomNumber = randomNumber + 1;");
+		//Casting the random number to an integer will round it down
+		//to an integer in the 1 to sides range.
+		System.out.println("return (int) randomNumber;");
+		System.out.println();
+		System.out.println("}");
+		System.out.println();
+		System.out.println("public int monopolyRoll() {");
+		System.out.println("int roll1 = diceRoll(6);");
+		System.out.println("int roll2 = diceRoll(6);");
+		System.out.println("int total = roll1 + roll2;");
+		System.out.println("while (roll1 == roll2) {");
+		System.out.println("roll1 = diceRoll(6);");
+		System.out.println("roll2 = diceRoll(6);");
+		System.out.println("total = total + roll1 + roll2;");
+		System.out.println("}");
+		System.out.println("return total;");
+		System.out.println();
 		
+		System.out.println("Answer to question#8, challange #1.");
+		System.out.println();
+		System.out.println("public int monopolyRoll() {");
+		System.out.println("int roll1 = diceRoll(6);");
+		System.out.println("int roll2 = diceRoll(6);");
+		System.out.println("int total = roll1 + roll2;");
+		//An extra variable is added to keep track of how many rolls
+		//have been made.
+		System.out.println("int rollsSoFar = 1;");
+		System.out.println("}");
+		System.out.println("int rollsSoFar = 1;");
+		System.out.println("while (roll1 == roll2) {");
+		//Here, we return -1 if doubles have been rolled too
+		//many times in a row.
+		System.out.println("if (rollsSoFar >= 3) return -1;");
+		System.out.println("roll1 = diceRoll(6);");
+		System.out.println("roll2 = diceRoll(6);");
+		System.out.println("total = total + roll1 + roll2;");
+		System.out.println("rollsSoFar = rollsSoFar + 1;");
+		System.out.println("}");
+		System.out.println("return total;");
+		System.out.println("}");
+		System.out.println();
 		
+		System.out.println("Answer to Question #8, challange #2.");
+		System.out.println();
+		System.out.println("public int monopolyRoll() {");
+		System.out.println("int roll1 = diceRoll(6);");
+		System.out.println("int roll2 = diceRoll(6);");
+		System.out.println("if (roll1 != roll2) {");
+		System.out.println("return roll1 + roll2;");
+		System.out.println("} else {");
+//In the case where the two rolls are equal, we want to\r\n" + 
+//return the current roll plus the return value of another\r\n" + 
+//call to monopolyRoll(). This is called making a\r\n" + 
+//recursive call. The recursive call will handle making\r\n" + 
+//additional rolls, and recursive calls will keep getting\r\n" + 
+//made until a roll is made where the two values are not\r\n" + 
+//equal.
+		System.out.println("return roll1 + roll2 + monopolyRoll();");
+		System.out.println("}");
+		System.out.println("}");
 		
 		
 		
